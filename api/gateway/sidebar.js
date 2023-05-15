e887ef4d8a4d7418ef62383c7ffccf7ffdf0f1c9
Amer <aalshabebi@dineropay.com>
2023-05-15 15:20:29 +0300

    updating api schema

--- api/gateway/sidebar.js
+++ api/gateway/sidebar.js
@@ -1,1 +1,45 @@
-module.exports = [{"type":"doc","id":"gateway/public-rest-api"},{"type":"category","label":"Purchase","items":[{"type":"doc","id":"gateway/authentication-request","label":"Authentication request to create a payment session","className":"api-method post"},{"type":"doc","id":"gateway/charge-token","label":"Charge a transaction using a saved token","className":"api-method post"}]},{"type":"category","label":"default","items":[{"type":"doc","id":"gateway/get-trans-status-request","label":"GET_TRANS_STATUS request","className":"api-method post"},{"type":"doc","id":"gateway/refund-request","label":"Refund request","className":"api-method post"}]}];
+module.exports = [
+	{ type: "doc", id: "gateway/public-rest-api" },
+	{
+		type: "category",
+		label: "Purchase",
+		items: [
+			{
+				type: "doc",
+				id: "gateway/authentication-request",
+				label: "Authentication request to create a payment session",
+				className: "gateway-method post",
+			},
+			{
+				type: "doc",
+				id: "gateway/charge-token",
+				label: "Charge a transaction using a saved token",
+				className: "gateway-method post",
+			},
+		],
+	},
+	{
+		type: "category",
+		label: "Status",
+		items: [
+			{
+				type: "doc",
+				id: "gateway/check-transaction-status",
+				label: "Get Transaction Status",
+				className: "gateway-method post",
+			},
+		],
+	},
+	{
+		type: "category",
+		label: "Refund",
+		items: [
+			{
+				type: "doc",
+				id: "gateway/refund",
+				label: "Refund request",
+				className: "gateway-method post",
+			},
+		],
+	},
+];
